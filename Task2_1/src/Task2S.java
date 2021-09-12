54e8b4dd9127b30719e5be8b55a05d02b02dbaaf
AlexandraKryvoruchko <sashshushu@gmail.com>
2021-09-12 19:59:40 +0300

    added  Task4

--- Task2_1/src/Task2S.java
+++ Task2_1/src/Task2S.java
@@ -5,9 +5,10 @@ public class Task2S {
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your name?");
         String name = scan.nextLine();
-        System.out.println("Where are you live, " + name +"?");
+        System.out.println("Where are you live, " + name + "?");
         String address = scan.nextLine();
         String wholeInformation = name + " - " + address;
         System.out.println(wholeInformation);
     }
 }
+
