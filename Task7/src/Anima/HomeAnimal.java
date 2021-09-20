66e1e0eb987502d2048ea9c281775e061a77207d
AlexandraKryvoruchko <sashshushu@gmail.com>
2021-09-21 00:10:18 +0300

    Task 7

---
+++ Task7/src/Anima/HomeAnimal.java
@@ -0,0 +1,9 @@
+package Anima;
+
+public abstract class HomeAnimal extends Animal {
+    String name;
+
+    void eat() {
+        System.out.println("Animal eats");
+    }
+}
---
+++ Task7/src/Anima/MainZoo.java
@@ -0,0 +1,28 @@
+package Anima;
+
+public class MainZoo {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog("Monica");
+        dog2.setHomeAddress("France, Paris");
+
+        System.out.println(dog2.name + " live in " + dog2.getHomeAddress() + " and has collar " + dog2.isCollar());
+        System.out.println(dog1.name + " live in " + dog1.getHomeAddress() + " and has collar " + dog1.isCollar());
+
+        Cat cat = new Cat("Murzik");
+
+        cat.move();
+        cat.eat();
+
+        Wolf wolf = new Wolf();
+        wolf.move();
+        wolf.hunter("Rabbit");
+
+        dog1.move();
+
+        dog1.eat();
+        dog2.eat(dog2.name);
+        dog2.eat("Sweet candy");
+    }
+}
